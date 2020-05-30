e41452ffae59ce4ccf254dc0b74f5aabcbe6d09e
Nikky Amresh <nikkyamresh8@gmail.com>
2020-05-30 08:14:01 +0530

    added link js_css to html
    Signed-off-by: Nikky Amresh <nikkyamresh8@gmail.com>

---
+++ web/link_css_js.py
@@ -0,0 +1,13 @@
+import io
+from glob import glob
+
+def addjscss(file):
+	with io.open(file,'r',encoding='utf-8') as f:
+		filedata=f.read()
+	filedata = filedata.replace('</head>','<link rel="stylesheet" type="text/css" href="style.css">\n\t</head>')
+	filedata = filedata.replace('</body>','<script type="text/javascript" src="script.js"></script>\n\t</body>')
+	with io.open(file,'w',encoding='utf-8') as f:
+		f.write(filedata)
+
+for file in glob("*/*.html"):
+	addjscss(file)
